b781156f29d6924c311ed294a2f37d5641fff1ad
Andrew Chilton <chilts@appsattic.com>
2012-07-09 20:01:53 +1200

    Change the upload program to use queues

--- bin/amazon-s3-upload.js
+++ bin/amazon-s3-upload.js
@@ -3,7 +3,10 @@
 
 var fs = require('fs');
 
+var fmt = require('fmt');
+var async = require('async');
 var inspect   = require('eyes').inspector();
+
 var awssum    = require('awssum');
 var amazon    = awssum.load('amazon/amazon');
 var S3        = awssum.load('amazon/s3').S3;
@@ -12,16 +15,31 @@ var accessKeyId     = process.env.ACCESS_KEY_ID;
 var secretAccessKey = process.env.SECRET_ACCESS_KEY;
 var awsAccountId    = process.env.AWS_ACCOUNT_ID;
 
+// --------------------------------------------------------------------------------------------------------------------
+
 var argv = require('optimist')
-    .usage('Usage: $0 --bucket name --filename filename.txt')
+    .usage('Usage: $0 --bucket name [--concurrency n] [files...]')
+
     .alias('bucket', 'b')
-    .alias('filename', 'f')
     .demand('b')
-    .demand('f')
     .describe('b', 'bucket name to store upload')
-    .describe('f', 'name of file to upload')
+
+    .alias('c', 'concurrency')
+    .default('c', 3)
+    .describe('c', 'concurrency level for uploads/downloads')
+
     .describe('debug', 'to turn debugging on')
+
     .argv;
+
+// --------------------------------------------------------------------------------------------------------------------
+
+fmt.sep();
+fmt.title('amazon-s3-upload.js');
+fmt.field('Bucket', argv.bucket);
+fmt.field('Concurrency', argv.concurrency);
+fmt.field('Files', argv._.length);
+fmt.line();
 
 // --------------------------------------------------------------------------------------------------------------------
 
@@ -32,64 +50,73 @@ var s3 = new S3({
     region          : amazon.US_EAST_1
 });
 
-console.log( 'Settings:');
-if ( argv.d || argv.debug ) {
-    console.log( '- Region          : ', s3.region() );
-    console.log( '- EndPoint        : ', s3.host() );
-    console.log( '- AccessKeyId     : ', s3.accessKeyId() );
-    console.log( '- SecretAccessKey : ', s3.secretAccessKey().substr(0,3) + "..." );
-    console.log( '- AwsAccountId    : ', s3.awsAccountId() );
-}
+// only upload the files that do not exist (unless --force is active)
+// var checkItemDoesNotExistQueue = async.queue(checkItemDoesNotExist, args.concurrency);
+// var checkMd5IsSameQueue = async.queue(checkMd5IsSame, args.concurrency);
+var statItemQueue   = async.queue(statItem,   argv.concurrency);
+var uploadItemQueue = async.queue(uploadItem, argv.concurrency);
 
-console.log( '- Bucket          : ', argv.bucket);
-console.log( '- Filename        : ', argv.filename);
-console.log();
-console.log(argv);
-console.log();
+// for all the files given on the command line, add them to the first queue
+argv._.forEach(function(filename, i) {
+    statItemQueue.push({
+        'filename' : filename,
+    });
+});
 
 // --------------------------------------------------------------------------------------------------------------------
 
-console.log('Statting File:');
-fs.stat(argv.filename, function(err, file_info) {
-    if (err) {
-        inspect(err, 'Error');
-        return;
-    }
-    console.log('- Size            : ', file_info.size);
-    console.log('- uid             : ', file_info.uid);
-    console.log('- gid             : ', file_info.gid);
-    console.log('- inode           : ', file_info.ino);
-    console.log('- ctime           : ', file_info.ctime);
-    console.log('- mtime           : ', file_info.mtime);
-    console.log();
-
-    // create a read stream
-    var bodyStream = fs.createReadStream( argv.filename );
-
-    var options = {
-        BucketName : argv.bucket,
-        ObjectName : argv.filename,
-        ContentLength : file_info.size,
-        Body : bodyStream
-    };
-
-    console.log('Putting Object:');
-    s3.PutObject(options, function(err, data) {
-        if (err) {
-            inspect(err, 'Error');
+function statItem(item, callback) {
+    // firstly, check if the file exists
+    fs.stat(item.filename, function(err, stats) {
+        if ( err ) {
+            // this file doesn't exist so we can't upload it
+            fmt.field('FileNotExist', item.filename);
+            callback();
             return;
         }
 
-        console.log('- Date            : ', data.Headers.date);
-        console.log('- Request Id      : ', data.Headers['x-amz-request-id']);
-        console.log('- Etag            : ', data.Headers.etag);
+        // we know there is a file
+        item.size = stats.size;
 
-        if ( argv.d || argv.debug ) {
-            console.log('- StatusCode      : ', data.StatusCode);
-            console.log('- Headers         : ');
-            inspect(data.Headers);
-        }
+        // add it to the upload queue
+        uploadItemQueue.push(item);
+        callback();
     });
-});
+}
+
+function uploadItem(item, callback) {
+    // create a read stream
+    var bodyStream = fs.createReadStream( item.filename );
+
+    var options = {
+        BucketName    : argv.bucket,
+        ObjectName    : item.filename,
+        ContentLength : item.size,
+        Body          : bodyStream
+    };
+
+    fmt.field('Uploading', item.filename + ' (' + item.size + ')');
+    s3.PutObject(options, function(err, data) {
+        if (err) {
+            fmt.field('UploadFailed', item.filename);
+
+            // put this item back on the queue if retries is less than the cut-off
+            if ( item.retries > 2 ) {
+                fmt.field('UploadCancelled', item.filename);
+            }
+            else {
+                // try again
+                item.retries = item.retries ? item.retries+1 : 1;
+                uploadItemQueue.push(item);
+            }
+
+            callback();
+            return;
+        }
+
+        fmt.field('Uploaded', item.filename);
+        callback();
+    });
+}
 
 // --------------------------------------------------------------------------------------------------------------------
